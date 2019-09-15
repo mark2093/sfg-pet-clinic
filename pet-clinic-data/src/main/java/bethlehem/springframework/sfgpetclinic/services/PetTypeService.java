f8ef2281a0819efc2134abef53e88bf94b7a4e11
mark2093 <markdsouza93@gmail.com>
2019-09-15 18:10:25 +0530

    Added PetTypeMap Service

---
+++ pet-clinic-data/src/main/java/bethlehem/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package bethlehem.springframework.sfgpetclinic.services;
+
+import bethlehem.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
