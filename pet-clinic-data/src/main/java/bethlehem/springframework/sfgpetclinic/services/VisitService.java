d90a6f361a8d8100ec53945e4dab6f3b6f02abef
Mark Dsouza <markdsouza93@gmail.com>
2020-07-13 18:16:14 +0530

    added spring data jpa ,map service for Visit

---
+++ pet-clinic-data/src/main/java/bethlehem/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package bethlehem.springframework.sfgpetclinic.services;
+
+import bethlehem.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
